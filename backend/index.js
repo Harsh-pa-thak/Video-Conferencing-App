0f8ab95a09b455d69f14d446c1034b2f6c597c16
Harsh Pathak <harphy45@gmail.com>
2026-05-23 17:17:56 +0530

    ejs

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,21 @@ import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
 import userModel from "./models/UserModel.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 
 configDotenv();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 
 const db = mongoose
@@ -23,7 +28,7 @@ const db = mongoose
   });
 
 app.get("/signup", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "public", "signup.html"));
+  res.render("signup");
 });
 app.post("/signup", async (req, res) => {
   const { name, username, password } = req.body;
@@ -31,11 +36,13 @@ app.post("/signup", async (req, res) => {
     if (!name || !username || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
-    if (userModel.find({ username }))
+    const existingUser = await userModel.findOne({ username });
+    if (existingUser)
       return res.status(400).json({ message: "Username already exists" });
     const newUser = new userModel({ name, username, password });
     await newUser.save();
-    res.status(201).json({ message: "User created successfully" });
+    
+    res.status(201).json({ message: "User created successfully" }).redirect("/user");
   } catch (e) {
     console.log(`Error creating user: ${e.message}`);
   }
